e0a6e117f63fe7b4c1298374e82c7fea7da4764c
elroi773 <s2457@e-mirim.hs.kr>
2026-01-30 00:05:58 +0900

    backup

---
+++ enough/src/contexts/AuthContext.jsx
@@ -0,0 +1,27 @@
+// src/contexts/AuthContext.jsx
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user); // 로그인 유저 or null
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
